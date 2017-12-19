cae69e947248296f1e830d8e610606deed924575
lemuelc7 <lemuelcco@gmail.com>
2017-12-19 14:23:26 +0100

    Create free and paid builds

--- jokedisplay/src/main/java/com/lemuel/lemubit/jokedisplay/jokeActivity.java
+++ jokedisplay/src/main/java/com/lemuel/lemubit/jokedisplay/jokeActivity.java
@@ -3,6 +3,8 @@ package com.lemuel.lemubit.jokedisplay;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class jokeActivity extends AppCompatActivity {
@@ -17,4 +19,22 @@ public class jokeActivity extends AppCompatActivity {
         jokeTxt = (TextView) findViewById(R.id.jokeTxt);
         jokeTxt.setText(joke);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.exit) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
