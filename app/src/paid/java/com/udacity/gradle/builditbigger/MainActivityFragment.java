d6bd6396356a7027aab515e50c396bd20e80d8c5
lemuelc7 <lemuelcco@gmail.com>
2017-12-19 13:28:33 +0100

    Create free and paid builds

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,50 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.lemuel.lemubit.jokedisplay.jokeActivity;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment implements OnJokeRecieved {
+
+    ProgressBar jokeProgressBar;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        Button jokeBtn = (Button) root.findViewById(R.id.tellJoke);
+        jokeProgressBar = (ProgressBar) root.findViewById(R.id.jokeProgressBar);
+        jokeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new EndpointsAsyncTask(MainActivityFragment.this).execute();
+                jokeProgressBar.setVisibility(View.VISIBLE);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void Joke(String joke) {
+        jokeProgressBar.setVisibility(View.INVISIBLE);
+        Intent intent = new Intent(getActivity(), jokeActivity.class);
+        Bundle mbundle = new Bundle();
+        mbundle.putString("joke", joke);
+        intent.putExtras(mbundle);
+        startActivity(intent);
+    }
+}
